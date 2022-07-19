d4350205caafcf8a903a4e5662112012198a3d31
Roksana Szajba <roksanaszajba@macbook-air-roksana.home>
2022-07-20 00:46:39 +0200

    new

---
+++ app.js
@@ -0,0 +1,31 @@
+
+// menu scroll
+document.addEventListener("DOMContentLoaded", function () {
+  const nav = document.querySelector(".navbar");
+
+  function addShadow() {
+    if (window.scrollY >= 100) {
+      nav.classList.add("shadow");
+    } else {
+      nav.classList.remove("shadow");
+    }
+  }
+
+  window.addEventListener('scroll',addShadow)
+});
+
+
+// scroll reval anime
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(
+  `.hero-text, .btn-dark,.about-img,.about-text,.title,.underline,.about-subtitle,.about-description,.card`,
+  {
+    interval: 100,
+  }
+);
